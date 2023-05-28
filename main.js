071a1fec0b83f104e62cd0f64ea26818fefacb1e
wityboss <88195937+wityboss@users.noreply.github.com>
2023-05-28 21:15:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var image = document.querySelector('#rony');
+var boom = document.querySelector('.hide')
 const audio = new Audio();
 audio.src = 'a30.mp3'
+
 image.onclick = function() {
     image.src = 'rony2.jpg';
     audio.play();
+    boom.style.display = 'block'
 };
 
